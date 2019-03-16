7a682d3a3b75921477e497832b37b27357333b96
msevilla <m.sevillazhou@gmail.com>
2019-03-16 13:45:51 +0100

    Added feature one page scroll with mouse

--- main.js
+++ main.js
@@ -1,86 +1,94 @@
 const sections = document.querySelectorAll('section');
 
-// const nextBtn = document.querySelector('.nextBtn');
-// const prevBtn = document.querySelector('.prevBtn');
-
 const section0Btn = document.querySelector('.section0Btn');
 const section1Btn = document.querySelector('.section1Btn');
 const section2Btn = document.querySelector('.section2Btn');
 const section3Btn = document.querySelector('.section3Btn');
+const idlePeriod = 200;
+const animationDuration = 1000;
 
+let lastAnimation = 0;
 let index = 0;
-// const animationDuration = 1000;
-// let lastTime = 0;
 
-// nextBtn.addEventListener('click', () => {
-//     index ++;
-//     sections.forEach((section, i) => {
-//         if (i === index) {
-//             section.scrollIntoView({behavior: 'smooth'});
-//         }
-//     })
-// });
+const toggleText = (index, state) => {
+    if (state === 'show') {
+      sections[index].querySelector('.text').classList.add('show');  
+    } else {
+      sections[index].querySelector('.text').classList.remove('show');  
+    } 
+}
+  
+toggleText(0, 'show');
 
-// prevBtn.addEventListener('click', () => {
-//     index --;
-//     sections.forEach((section, i) => {
-//         if (i === index) {
-//             section.scrollIntoView({behavior: 'smooth'});
-//         }
-//     })
-// });
+document.addEventListener('wheel', event => {
+    var delta = event.wheelDelta;
+    var timeNow = new Date().getTime();
+    // Cancel scroll if currently animating or within quiet period
+    if(timeNow - lastAnimation < idlePeriod + animationDuration) {
+      event.preventDefault();
+      return;
+    }
+    // scrolling down
+    if (delta < 0) {
+        if (index > 2) return;
+        toggleText(index, 'hide');
+        index++;
 
-// window.addEventListener('wheel', (e) => {
-//     const delta = e.wheelDelta;
-//     const currentTime = new Date().getTime();
+        sections.forEach((section, i) => {       
+            if (i === index) {
+            toggleText(i, 'show');
+            section.scrollIntoView({behavior: "smooth"});
+        }
+        })
 
-//     if (currentTime - lastTime < animationDuration) {
-//         e.preventDefault();
-//         return;
-//     }
-
-//     if (delta < 0) {
-//         const nextBtnClick = new Event('click');
-//         nextBtn.dispatchEvent(nextBtnClick);
-//     } else {
-//         const prevtBtnClick = new Event('click');
-//         prevBtn.dispatchEvent(prevBtnClick);
-//     }
-//     lastTime = currentTime;
-// })
+    // scrolling up
+    } else {
+        if (index < 1) return;
+        toggleText(index, 'hide');
+        index--;
+        
+        sections.forEach((section, i) => {
+            if (i === index) {
+            toggleText(i, 'show');
+            section.scrollIntoView({behavior: "smooth"});
+        }
+        })
+    }
+    
+    lastAnimation = timeNow;
+})
 
 section0Btn.addEventListener('click', () => {
     index = 0;
-    sections.forEach((section, i) => {
-        if (i === index) {
-            section.scrollIntoView({behavior: 'smooth'});
-        }
-    })
-});
+    sections[index].scrollIntoView({block: "start", behavior: 'smooth'});
+    toggleText(index, 'show');
+})
 
 section1Btn.addEventListener('click', () => {
     index = 1;
-    sections.forEach((section, i) => {
-        if (i === index) {
-            section.scrollIntoView({behavior: 'smooth'});
-        }
-    })
-});
+    sections[index].scrollIntoView({block: "start", behavior: 'smooth'});
+    toggleText(index, 'show');
+})
 
 section2Btn.addEventListener('click', () => {
     index = 2;
-    sections.forEach((section, i) => {
-        if (i === index) {
-            section.scrollIntoView({behavior: 'smooth'});
-        }
-    })
-});
+    sections[index].scrollIntoView({behavior: 'smooth'});
+    toggleText(index, 'show');
+})
 
 section3Btn.addEventListener('click', () => {
     index = 3;
-    sections.forEach((section, i) => {
-        if (i === index) {
-            section.scrollIntoView({behavior: 'smooth'});
-        }
-    })
+    sections[index].scrollIntoView({behavior: 'smooth'});
+    toggleText(index, 'show');
 });
+
+// SOLUTION WITH LOOP
+
+//section1Btn.addEventListener('click', () => {
+//     index = 1;
+//     sections.forEach((section, i) => {
+//         if (i === index) {
+//             section.scrollIntoView({behavior: 'smooth'});
+//         }
+//     })
+// });
