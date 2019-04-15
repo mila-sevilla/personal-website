a5e70fc790a0edc9d965108a329a64269a8fa59d
msevilla <m.sevillazhou@gmail.com>
2019-04-15 17:00:20 +0200

    Added hmtl & css for image slider

--- src/main.js
+++ src/main.js
@@ -1,1 +1,46 @@
 import "./main.scss";
+
+let slides = document.querySelectorAll('.slide');
+let nextBtn = document.querySelector('#nextBtn');
+let prevBtn = document.querySelector('#prevBtn');
+let current = 0;
+
+function reset() {
+    for(let i = 0; i < slides.length; i++) {
+        slides[i].getElementsByClassName.display = 'none';
+    }
+}
+
+function startSlide() {
+    reset();
+    slides[0].getElementsByClassName.display = 'block';
+}
+
+function slidePrev() {
+    reset();
+    slides[current - 1].style.display = 'block';
+    current--;
+}
+
+function slideNext() {
+    reset();
+    slides[current + 1].style.display = 'block';
+    current++;
+}
+
+prevBtn.addEventListener('click', function() {
+    if(current === 0) {
+        current = slides.length;
+    }
+    slidePrev();
+})
+
+nextBtn.addEventListener('click', function() {
+    if(current === slides.length - 1) {
+        current = -1;
+    }
+    slideNext();
+})
+
+startSlide();
+
