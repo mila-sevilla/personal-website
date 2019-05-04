73c129118349d380fa8964ece7bb7eb378457a5c
msevilla <m.sevillazhou@gmail.com>
2019-05-04 15:50:08 +0200

    Code refactoring

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-import './styles/main.scss'
-
 // Portfolio Carousel
 
 //Necessary for IE, Array.from not supported:
@@ -68,47 +66,3 @@ dots.forEach(
     )
   }
 ) 
-
-
-// Full page scroll on desktop
-// const sections = document.querySelectorAll('section');
-// // const section0Link = document.querySelector('.section0Link');
-// // const section1Link = document.querySelector('.section1Link');
-// // const section2Link = document.querySelector('.section2Link');
-// // const section3Link = document.querySelector('.section3Link');
-// const idlePeriod = 200;
-// const animationDuration = 1000;
-// let lastAnimation = 0;
-// let index = 0;
-
-// document.addEventListener('wheel', event => {
-//     var delta = event.wheelDelta;
-//     var timeNow = new Date().getTime();
-//     // Cancel scroll if currently animating or within quiet period
-//     if (timeNow - lastAnimation < idlePeriod + animationDuration) {
-//       event.preventDefault();
-//       return;
-//     }
-//     // scrolling down
-//     if (delta < 0) {
-//         if (index > 2) return;
-//         index++;
-
-//         sections.forEach((section, i) => {       
-//             if (i === index) {
-//             section.scrollIntoView({behavior: "smooth"});
-//             }
-//         })
-//     // scrolling up
-//     } else {
-//         if (index < 1) return;
-//         index--;
-        
-//         sections.forEach((section, i) => {
-//             if (i === index) {
-//             section.scrollIntoView({behavior: "smooth"});
-//             }
-//         })
-//     }  
-//     lastAnimation = timeNow;
-// })
